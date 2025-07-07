bb6d50f7aabd8e6e4073352926c822b116e3f085
Stephen Cross <scross01@gmail.com>
2025-07-07 05:42:32 -0400

    update formatting

--- chatbridge/main.py
+++ chatbridge/main.py
@@ -55,7 +55,9 @@ logging.getLogger("sse_starlette").setLevel(
 
 api_key = os.getenv("API_KEY", None)
 if not api_key or api_key == "":
-    logger.warning("API_KEY is not configured, access is open to unauthenticated clients.")
+    logger.warning(
+        "API_KEY is not configured, access is open to unauthenticated clients."
+    )
 
 security = HTTPBearer()
 
